a80552855727a4f0250bac0740574bf684997e04
liantsoa <hei.liantsoa@gmail.com>
2024-08-09 21:20:06 +0200

    feat : finished the project

--- src/calculateWithAmortissement.js
+++ src/calculateWithAmortissement.js
@@ -1,4 +1,10 @@
-function calculateValue(valeur, dateDebut, tauxAmortissement, valeurConstante, actualDate) {
+function calculateValue(
+  valeur,
+  dateDebut,
+  tauxAmortissement,
+  valeurConstante,
+  actualDate
+) {
   const dateActuelle = actualDate ? new Date(actualDate) : new Date();
   const dateDebutObj = new Date(dateDebut);
 
@@ -8,19 +14,20 @@ function calculateValue(valeur, dateDebut, tauxAmortissement, valeurConstante, a
 
   const diffEnMillis = dateActuelle - dateDebutObj;
   const differenceDay = Math.floor(diffEnMillis / (1000 * 60 * 60 * 24));
-  const differenceMonth = dateActuelle.getMonth() - dateDebutObj.getMonth() + (12 * (dateActuelle.getFullYear() - dateDebutObj.getFullYear()));
-  const differenceYear = dateActuelle.getFullYear() - dateDebutObj.getFullYear();
+  const differenceMonth =
+    dateActuelle.getMonth() -
+    dateDebutObj.getMonth() +
+    12 * (dateActuelle.getFullYear() - dateDebutObj.getFullYear());
+  const differenceYear =
+    dateActuelle.getFullYear() - dateDebutObj.getFullYear();
 
   if (!tauxAmortissement) {
     const result = valeurConstante * differenceMonth;
     return result;
   } else {
-    var raison =
-      differenceYear +
-      differenceMonth / 12 +
-      differenceDay / 365;
+    var raison = differenceYear + differenceMonth / 12 + differenceDay / 365;
     const result = valeur - valeur * ((raison * tauxAmortissement) / 100);
-    return result;
+    return Math.floor(result);
   }
 }
 
--- src/table.js
+++ src/table.js
@@ -3,9 +3,30 @@ import Table from "react-bootstrap/Table";
 import axios from "axios";
 import formatDate from "./formatDate";
 import calculateValue from "./calculateWithAmortissement";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Button from "react-bootstrap/Button";
+import "./table.css";
 
 function ShowTable() {
   const [patrimoine, setPatrimoine] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [patrimoineTotal, setPatrimoineTotal] = useState();
+
+  function totalPatrimoine() {
+    let total = 0;
+    patrimoine.forEach((possession) => {
+      const result = calculateValue(
+        possession?.valeur,
+        possession?.dateDebut,
+        possession?.tauxAmortissement,
+        possession?.valeurConstante,
+        startDate
+      );
+      total += result;
+    });
+    setPatrimoineTotal(Math.floor(total) + " Ar");
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,32 +52,59 @@ function ShowTable() {
   }, []);
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>libelle</th>
-          <th>valeur initiale</th>
-          <th>date de debut</th>
-          <th>date de fin</th>
-          <th>amortissement</th>
-          <th>valeur actuelle</th>
-        </tr>
-      </thead>
-      <tbody>
-        {patrimoine.map((possession, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{possession?.libelle}</td>
-            <td>{possession?.valeur}</td>
-            <td>{formatDate(possession?.dateDebut)}</td>
-            <td>{possession?.dateFin}</td>
-            <td>{possession?.tauxAmortissement}</td>
-            <td>{calculateValue(possession?.valeur, possession?.dateDebut, possession?.tauxAmortissement, possession?.valeurConstante, null)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <div className="container">
+      <div className="table-container">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>libelle</th>
+              <th>valeur initiale</th>
+              <th>date de debut</th>
+              <th>date de fin</th>
+              <th>amortissement</th>
+              <th>valeur actuelle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patrimoine.map((possession, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{possession?.libelle}</td>
+                <td>{possession?.valeur}</td>
+                <td>{formatDate(possession?.dateDebut)}</td>
+                <td>{possession?.dateFin}</td>
+                <td>{possession?.tauxAmortissement}</td>
+                <td>
+                  {calculateValue(
+                    possession?.valeur,
+                    possession?.dateDebut,
+                    possession?.tauxAmortissement,
+                    possession?.valeurConstante,
+                    null
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <div className="date-picker-container">
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => {
+            const newDate = new Date(date);
+            setStartDate(newDate);
+          }}
+        />
+        <Button variant="success" onClick={totalPatrimoine}>
+          Valider
+        </Button>
+      </div>
+      <div className="text-container">
+        <p>Votre patrimoine est: {patrimoineTotal} </p>
+      </div>
+    </div>
   );
 }
 
